d391162a527ec56f5ec2ac62698795a6193f4f7b
Sakis T. <106918030+AthTop@users.noreply.github.com>
2024-11-05 16:25:52 +0200

    Implement more functions

--- bst.js
+++ bst.js
@@ -39,4 +39,83 @@ export default class Tree {
     }
     return uniqueArray; // return the new sorted and unique array
   }
+
+  // Inserts value in BST
+  insert(data, root = this.root) {
+    // Base case to exit recursion
+    if (data === root.data) return;
+    // Check if new data is smaller, then check if the left is empty and append it,
+    // else recursively call insert until leaf node is found
+    if (data < root.data) {
+      if (root.left === null) {
+        root.left = new Node(data);
+      } else {
+        this.insert(data, root.left);
+      }
+      // Similarilly, check if data is bigger and check the right side as above
+    } else {
+      if (root.right > root.data) {
+        if (root.right === null) {
+          root.right = new Node(data);
+        } else {
+          this.insert(data, root.right);
+        }
+      }
+    }
+  }
+  deleteItem(data, root = this.root) {
+    // Base case
+    if (root === null) return root;
+    //Check if we need to go to left or right of root
+    if (data < root.data) {
+      root.left = this.deleteItem(data, root.left);
+    } else if (data > root.data) {
+      root.right = this.deleteItem(data, root.right);
+    }
+    // If root matches our data
+    else {
+      // Check if no left child or only right
+      if (root.left === null) {
+        return root.right;
+      }
+      if (root.right === null) {
+        return root.left;
+      }
+      // Case with 2 children
+      let successor = this.#getSuccessor(root);
+      root.data = successor.data;
+      root.right = this.deleteItem(successor.data, root.right);
+    }
+    return root;
+  }
+  // Helper function to get the left-most node to the right of a node
+  #getSuccessor(root) {
+    root = root.right;
+    while (root !== null && root.left !== null) {
+      root = root.left;
+    }
+    return root;
+  }
+  find(data, root = this.root) {
+    // Base case
+    if (root === null || root.data === data) return root;
+    if (data < root.data) {
+      return this.find(data, root.left);
+    }
+    if (data > root.data) {
+      return this.find(data, root.right);
+    }
+  }
+  levelOrder(callback, queue = [this.root]) {
+    if (typeof callback !== "function") {
+      throw new Error("Callback function is required");
+    }
+    if (root === null) return;
+    if (queue.length === 0) return;
+    let node = queue.shift();
+    callback(node);
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+    this.levelOrder(callback, queue);
+  }
 }
